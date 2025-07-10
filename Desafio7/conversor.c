f17c2f87a55fc524a8b6cd6074ef5046622e6505
Fernanda Ferreira <159725027+FerreiraNanda@users.noreply.github.com>
2025-07-10 12:54:10 -0300

    Add files via upload

---
+++ Desafio7/conversor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+
+    printf("Digite a temperatura em Celsius: ");
+    scanf("%f", &celsius);
+
+
+    fahrenheit = (celsius * 9.0 / 5.0) + 32;
+
+    printf("Temperatura em Fahrenheit: %.2f ºF\n", fahrenheit);
+
+    return 0;
+}
+
